9266e79ec5b88eb58029a5c6d3e3756cff5ef77a
D.P.N.P. Altorf <daniel.altorf@hva.nl>
2019-10-14 10:36:32 +0200

    Init project.

---
+++ app/src/main/java/com/example/rockpaperscissors/database/GameRepository.kt
@@ -0,0 +1,22 @@
+package com.example.rockpaperscissors.database
+
+import android.content.Context
+import com.example.rockpaperscissors.model.Game
+
+class GameRepository(context: Context) {
+    private var gameDao: GameDao
+
+    init {
+        val gameRoomDatabase = GameRoomDatabase.getDatabase(context)
+        gameDao = gameRoomDatabase!!.gameDao()
+    }
+
+    suspend fun getAllGames(): List<Game> = gameDao.getAllGames()
+
+    suspend fun insertGame(game: Game) = gameDao.insertGame(game)
+
+//    suspend fun deleteProduct(product: Product) = gameDao.deleteProduct(game)
+
+    suspend fun deleteAllGames() = gameDao.deleteAllGames()
+
+}
